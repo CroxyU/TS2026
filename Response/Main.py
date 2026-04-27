83102b0e6be03481e08d4a36f9e3b06e8feb32bc
varandystany <asvdot2010@gmail.com>
2026-04-27 17:02:12 +0300

    16

--- Response/Main.py
+++ Response/Main.py
@@ -1,8 +1,5 @@
 from core.Central import Central
 from core.Requests import *
 
-Persons = {"Angela" : Central(0, True, 36, "Анжела"),
-           "Meredict" : Central(0, True, 46, "Мередикт"),
-           "Sesil" : Central(0, True, 56, "Сесиль"),
-           "Fillip" : Central(0, True, 46, "Филип"),
-           "Elsa" : Central(0, True, 46, "Эльза"), }
+def Work(Name):
+    
