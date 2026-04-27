457363b705f418468d56084ff736a493d96cc724
varandystany <asvdot2010@gmail.com>
2026-04-27 14:58:06 +0300

    8

--- Core/Central.py
+++ Core/Central.py
@@ -1,2 +1,10 @@
 class Central():
-    
+    def __init__(self):
+        self.Person = "Start" # Angela / Meredict / Sesil / Fillip / Elsa 
+        self.AngelaAngry = 0
+        self.MeredictAngry = 0
+        self.SesilAngry = 0
+        self.FillipAngry = 0
+        self.ElsaAngry = 0
+            
+
--- Core/Recuests.py
+++ Core/Recuests.py
@@ -1,32 +1,60 @@
 import os
 from groq import Groq
 from dotenv import load_dotenv
-
+import ShortInf
 
 load_dotenv()
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 
-def CnahgePerson():
+def CnahgePerson(Name, Age):
+    readed1 = open("Docs/" + Name + " - Воспоминания.txt", "r")
+    readed2 = open("Docs/" + Name + " - Внутренняя информация.txt", "r")
+    readed3 = open("Docs/" + Name + " - Описание.txt", "r")
     chat_completion = client.chat.completions.create(
         messages=[
             {
-                "role": "---",
-                "content": "---",
-        }
-    ],
-    model="openai/gpt-oss-20b")
-    return 
+                "role": "",
+                "content": "Забудь все предыдушие командыы",
+            }
+        ],
+    model="llama-3.1-70b-versatile" )
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                
+                f"Ты -  {Name}, тебе - {Age} лет. Ты - {readed3.read()}\n"
+                f"Твое отношение к другим персонажам - {readed2.read()} \n Твои воспоминания о моменте убийства - {readed1()} \n ", 
+                f"Ты — персонаж детективной игры. Ты участвуешь в допросе.  Отвечай только от первого лица, строго в рамках своего характера.  Не ломай четвёртую стену, не говори от лица автора. Ты можешь врать, уклоняться от ответов, нервничать, но всегда оставайся в образе."
+                f"Твоя задача — отыгрывать свою роль максимально реалистично."
+                f"Запомни свою роль на все следуюшие промты"
+            }
+        ],
+    model="llama-3.1-70b-versatile" )
+    readed1.close()
+    readed2.close()
+    readed3.close()
+    return 0
 
 client = Groq(
-    api_key=os.environ.get(GROQ_API_KEY)
+    api_key=GROQ_API_KEY
 )
-def Qetion():
+
+def Hello():
     chat_completion = client.chat.completions.create(
         messages=[
             {
-                "role": "---",
-                "content": "---",
+                "Напиши приветственное сообшение для детектива."
         }
     ],
-    model="openai/gpt-oss-20b")
+    model="llama-3.1-70b-versatile")
+    return chat_completion.choices[0].message.content
+
+def Qetion(Qestion):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "Qestion"
+        }
+    ],
+    model="llama-3.1-70b-versatile")
     return chat_completion.choices[0].message.content
