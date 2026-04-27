fe3a5c8e4011c2dfd411ed8761979a6aeb3d5935
varandystany <asvdot2010@gmail.com>
2026-04-27 22:44:24 +0300

    20

--- Response/Main.py
+++ Response/Main.py
@@ -1,5 +1,17 @@
 from core.Central import Central
-from core.Requests import *
+from Response.WorkWithHTML import Persons
+from core.Requests import Answer
+from core.Requests import ChangePerson
 
-def Work(Name):
+def Work(Name, Quetion):
+    Persons[Name].ConversationHistory.append(Quetion)
+    if Persons[Name].IsActive:
+        Ans = Answer(Persons[Name].ConversationHistory)
+        Persons[Name].ConversationHistory.append(Ans)
+        
+    elif not Persons[Name].IsActive:
+            Persons[Name].ConversationHistory.append(ChangePerson(Name))
+            Ans = Answer(Persons[Name].ConversationHistory)
+            Persons[Name].ConversationHistory.append(Ans)
+    return Ans 
     
