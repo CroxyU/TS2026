c7750c5c4b48aa89663df49676adb394e9d33aa2
Dmitriy <grow54YT@gmail.com>
2026-04-27 14:58:58 +0300

    9

--- Core/Recuests.py
+++ Core/Recuests.py
@@ -1,32 +1,4 @@
-import os
 from groq import Groq
-from dotenv import load_dotenv
+from Core.config import GROQ_API_KEY
 
-
-load_dotenv()
-GROQ_API_KEY = os.getenv('GROQ_API_KEY')
-
-def CnahgePerson():
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "---",
-                "content": "---",
-        }
-    ],
-    model="openai/gpt-oss-20b")
-    return 
-
-client = Groq(
-    api_key=os.environ.get(GROQ_API_KEY)
-)
-def Qetion():
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "---",
-                "content": "---",
-        }
-    ],
-    model="openai/gpt-oss-20b")
-    return chat_completion.choices[0].message.content
+client = Groq(api_key=GROQ_API_KEY)
