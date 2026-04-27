236a367bffd9e527a3b7010fb2432c3c82c0dddc
varandystany <asvdot2010@gmail.com>
2026-04-27 12:30:52 +0300

    3

--- Core/Recuests.py
+++ Core/Recuests.py
@@ -1,10 +1,14 @@
 import os
 from groq import Groq
-import keys.env
+import env
 from dotenv import load_dotenv
 
+
+load_dotenv()
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+
 client = Groq(
-    api_key=os.environ.get("GROQ_API_KEY")
+    api_key=os.environ.get(GROQ_API_KEY)
 )
 
 chat_completion = client.chat.completions.create(
