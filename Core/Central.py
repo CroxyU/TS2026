e97930088c218f479fa0e19abfd306597e3fa0d7
varandystany <asvdot2010@gmail.com>
2026-04-27 16:00:53 +0300

    12

--- Core/Central.py
+++ Core/Central.py
@@ -1,16 +1,15 @@
 class Central():
     def __init__(self):
-        self.Person = "Start" # Angela / Meredict / Sesil / Fillip / Elsa 
-        self.AngelaAngry = 0
-        self.MeredictAngry = 0
-        self.SesilAngry = 0
-        self.FillipAngry = 0
-        self.ElsaAngry = 0
+        # self.Person = "Start" # Angela / Meredict / Sesil / Fillip / Elsa 
+        # self.Angela = {"Angry":0, "IsWork" : True, "Age" : 38, "Name" : "Анжела"}
+        # self.Meredict = {"Angry":0, "IsWork" : True, "Age" : 38, "Name" : "Мередикт"}
+        # self.Sesil = {"Angry":0, "IsWork" : True, "Age" : 38, "Name" : "Сесиль"}
+        # self.Fillip = {"Angry":0, "IsWork" : True, "Age" : 38, "Name" : "Филип"}
+        # self.Elsa = {"Angry":0, "IsWork" : True, "Age" : 38, "Name" : "Эльза"}
+        self.Angry = Angry
+        self.IsWork = Work
+        self.Age = Age
+        self.Name = Name 
+        self.ConversationHistory = []
 
-        self.AngelaAge = 46
-        self.MeredictAge = 46
-        self.SesilAge = 46
-        self.FillipAge = 46
-        self.ElsaAge = 46
-            
 
